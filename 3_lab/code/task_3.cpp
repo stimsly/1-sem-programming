a1847f22bfda8ee58900acb0e61172025452d377
Valera <valaignat@gmail.com>
2021-10-20 14:48:16 +0300

    complete 3 lab

--- 3_lab/code/task_3.cpp
+++ 3_lab/code/task_3.cpp
@@ -2,25 +2,16 @@
 
 using namespace std;
 
-int count(long long a) {
-    int k = 0;
-    while (a) {
-        a /= 10;
-        k++;
-    }
-    return max(1, k);
-}
-
 int main() {
-    
+
 
     cout << "Author: Ignatiev Valerii Alekseevich 1309\n"
         << "Version 3.1.1\n"
         << "Start: 19.10.2021\n"
         << "End: 19.10.2021\n\n";
 
-    const double emin = 0.000001,
-        emax = 1e10;
+    double emin = 0.000001,
+        emax = 1e200;
 
     cout << "  n  \n";
     cout << " ---\n";
@@ -34,7 +25,9 @@ int main() {
     cout << "Enter x[-1e6, 1e6][7d.12d]: ";
     cin >> x;
     cout << endl;
-
+    cout << "Enter emin: ";
+    cin >> emin;
+    cout << endl;
     double x2 = x;
 
     ofstream fout("output.txt");
@@ -45,62 +38,57 @@ int main() {
     fout << " ---     (3i)!\n";
     fout << "i = 1\n\n";
 
-    double cur = 1, sum = 0;
-    cout << "-----------------------------------------------------------\n";
-    cout << "|Numb| Current value              | Sum                   |\n";
-    cout << "-----------------------------------------------------------\n";
-    fout << "-----------------------------------------------------------\n";
-    fout << "|Numb| Current value              | Sum                   |\n";
-    fout << "-----------------------------------------------------------\n";
-    for (int i = 1; i <= 999; i++) {
-        double k = (i + 2) * (i + 3);
-        k /= (2.0 * i + 2);
-        k /= (2.0 * i + 3);
-        cur *= k;
-        cur *= x;
-        sum += cur;
-        x *= x2;
-        int d1 = 12 - count(cur);
-        int d2 = 12 - count(sum);
-        int d3 = 4 - count(i);
-        if (cur < 0) d1--;
-        if (sum < 0) d2--;
-        printf("|%4d|", i);
-        fout << "|";
-        while (d3--) fout << " ";
-        fout << i << "|";
-        while (d1--) {
-            cout << " ";
-            fout << " ";
+    double cur = 1, sum = 1;
+    cout << "-----------------------------------------------------------------------\n";
+    cout << "| Numb| Current value                | Sum                            |\n";
+    cout << "-----------------------------------------------------------------------\n";
+    fout << "-----------------------------------------------------------------------\n";
+    fout << "| Numb| Current value                | Sum                            |\n";
+    fout << "-----------------------------------------------------------------------\n";
+    for (long long i = 1; i <= 999; i++) {
+        if (i == 999) {
+            cout << "Out of i\n";
+            fout << "Out of i\n";
+            break;
         }
-        fout << fixed << setprecision(15) << cur << "|";
-        printf("%.15f|", cur);
-        while (d2--) {
-            cout << " ";
-            fout << " ";
-        }
-        printf("%.10f|\n", sum);
-        fout << fixed << setprecision(10) << sum << "|\n";
-        cout << "-----------------------------------------------------------\n";
-        fout << "-----------------------------------------------------------\n";
-
         if (fabs(cur) <= emin) {
             cout << "\nCurrent value is less then emin.\n";
             fout << "\nCurrent value is less then emin.\n";
             break;
         }
         if (fabs(cur) >= emax) {
-            cout << "\nCurrent value is bigger then emin.\n";
-            fout << "\nCurrent value is bigger then emin.\n";
+            cout << "\nCurrent value is bigger then emax.\n";
+            fout << "\nCurrent value is bigger then emax.\n";
             break;
         }
         if (fabs(sum) >= emax) {
-            cout << "\nSum is bigger then emin.\n";
-            fout << "\nSum is bigger then emin.\n";
+            cout << "\nSum is bigger then emax.\n";
+            fout << "\nSum is bigger then emax.\n";
             break;
         }
-       
+        cout << "|" << left << scientific << setw(5) << i << "|"
+            << setw(30) << setprecision(20) << cur << "|"
+            << setw(32) << setprecision(20) << sum << "|\n";
+        cout << "-----------------------------------------------------------------------\n";
+        fout << "|" << left << scientific << setw(5) << i << "|"
+            << setw(30) << setprecision(20) << cur << "|"
+            << setw(32) << setprecision(20) << sum << "|\n";
+        fout << "-----------------------------------------------------------------------\n";
+        if (i % 10 == 0) {
 
+            cout << "| Numb| Current value                | Sum                            |\n";
+            cout << "-----------------------------------------------------------------------\n";
+        }
+        double k = (2.0 * i + 2) * (2.0 * i + 3);
+        k /= (3.0 * i + 1);
+        k /= (3.0 * i + 2);
+        k /= 3.0;
+        cur *= k;
+        cur *= x;
+        sum += cur;
+        x *= x2;
+        
+       
     }
 
 
