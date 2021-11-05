25e83f6457ebcd3ec13d27a593c886cb0ea97b42
stimsly <valaignat@gmail.com>
2021-11-05 17:36:36 +0300

    add code for 5th lab

---
+++ 5_lab/code/lab5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+	
+	ifstream fin("input.txt");
+	ofstream fout("output.txt");
+	int t;
+	fin >> t;
+	while(t--){
+			int n;
+			fin >> n;
+			int a[n][n];
+			for(int i = 0; i < n; i++){
+				for(int j = 0; j < n; j++) fin >> a[i][j];
+			}
+			
+			
+			
+			for(int j = 0; j < n; j++){
+				for(int i = n - 1; i >= 0; i--){
+					fout << a[i][j] << " ";
+					
+				}
+				fout << endl;
+			}
+			fout << endl;
+		
+			
+	}
+}
+
