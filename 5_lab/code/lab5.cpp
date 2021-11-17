8150daa46e57c65c99b7a4cd14f96be89d9165b9
stimsly <valaignat@gmail.com>
2021-11-17 11:04:30 +0300

    complete lab_5

--- 5_lab/code/lab5.cpp
+++ 5_lab/code/lab5.cpp
@@ -7,25 +7,26 @@ ifstream fin("input.txt");
 ofstream fout("output.txt");
 
 const int empty_error = -999999;
-
+const int nmax = 100;
 
 	
-int read_one_int();
+int read_one_int(int &y);
 
 void solve(int t);
 void incorrect_test_case(int t);
 void print_matrix(int a[], int n, int type);
+void render_array(int a[], int n, int k);
 void clockewise(int a[], int n);
+void skip();
 
 int main(){
-	
-	int t = read_one_int();
-	
+	int y = 0;
+	int t = read_one_int(y);
+	if(!y) skip();
 	if(t == -999999){
 		fout << "Empty input!\n";
 		return 0;
 	}
-	cout << t << endl;
 	for(int i = 1; i <= t; i++){
 			solve(i);
 	}
@@ -33,33 +34,50 @@ int main(){
 }
 
 void solve(int t){
-	int n = read_one_int();
-	
+	int y = 0;
+	int n = read_one_int(y);
+	int k = n;
+	if(!y) skip();
+	if(n <= 0 or n >= nmax){
+		incorrect_test_case(t);
+		return;
+	}
 	int *a = new int[n * n];
 	
+	fout << "Test case: #" << t << endl;
+	
+	fout << "Initial: \n";
+	fout << n << endl;
 	for(int i = 0; i < n; i++){
 		int current = 0;
 		int j = 0;
-		
-		while(1){
-			current = read_one_int();
+		y = 0;
+		while(j < n){
+			current = read_one_int(y);
 			if(current == empty_error) break;
-			a[i * n + j] = current;
+			a[i * n + j++] = current;
+			fout << current << " ";
 		}
-		n = min(n, j);
+		fout << endl;
+		if(!y) skip();
+		k = min(k, j);
 	}
+	
+	render_array(a, n, k);
+	n = k;
+	
 	if(n <= 0){
 		incorrect_test_case(t);
 		return;
 	}
 	
-	fout << "Test case: " << t << endl;
-	
 	print_matrix(a, n, 1);
 	
-	clockewise(a, n);
+	for(int i = 0; i < 3; i++)clockewise(a, n);
 	
 	print_matrix(a, n, 2);
+	
+	fout << "Succses\n\n";
 	
 	return;
 	
@@ -75,7 +93,7 @@ void clockewise(int a[], int n){
 		for(int j = i; j < n - 1 - i; j++){
 			int t = a[i * n + j];
 			a[i * n + j] = a[j * n + n - 1 - i];
-			a[j * n + n - 1 - j] = a[(n-1-i) * n + n - 1 - j];
+			a[j * n + n - 1 - i] = a[(n-1-i) * n + n - 1 - j];
 			a[(n-1-i) * n + n - 1 - j] = a[(n-1-j) * n + i];
 			a[(n-1-j) * n + i] = t; 
 		}
@@ -83,16 +101,22 @@ void clockewise(int a[], int n){
 	return;
 }
 
-int read_one_int(){
+void render_array(int a[], int n, int k){
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < k; j++) a[i*k+j] = a[i*n+j];
+	}
+}
+
+int read_one_int(int &y){
 	/// -999999 = end of line
 	
 	char c;
 	int x = 0;
 	int res = 0;
-	while(fin >> c){
-	cout << c << " ";
+	while(!fin.eof()){
+		c = fin.get();
 		if(c == '\n'){
-			res = empty_error;
+			y = 1;
 			break;
 		}
 		if(c <= '9' and c >= '0'){
@@ -105,18 +129,24 @@ int read_one_int(){
 }
 
 void incorrect_test_case(int t){
-	fout << "Test case number: " << t << " is incorrect\n\n";
+	fout << "Test case number: #" << t << " is incorrect\n\n";
 }
 
 void print_matrix(int a[], int n, int type){
-cout << n << " " << type << endl;
 	if(type == 1)fout << "\nPrimary:\n";
 	else fout << "\nSecondary:\n";
+	fout << n << endl;
 	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++) cout << a[i*n+j] << " ";
-		cout << endl;
+		for(int j = 0; j < n; j++) fout << a[i*n+j] << " ";
+		fout << endl;
 	}
-	cout << endl;
+	fout << endl;
 }
 
-
+void skip(){
+	char c;
+	while(1){
+		c = fin.get();
+		if(c == '\n') break;
+	}
+}
